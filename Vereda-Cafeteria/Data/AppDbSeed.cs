0cbd60f21c25e1670b500369db3a7ac32162f5ae
bgsana <bgs.ana08@gmail.com>
2026-05-05 22:31:04 -0300

    Finalizada a página "Nosso Ambiente" e iniciado dashboard da área administrativa

--- Vereda-Cafeteria/Data/AppDbSeed.cs
+++ Vereda-Cafeteria/Data/AppDbSeed.cs
@@ -105,7 +105,7 @@ public class AppDbSeed
 
         builder.Entity<Banner>().HasData(banners);
         #endregion
-    
+
         #region Popular Roles - Tipos de Perfis de Usuário
         List<IdentityRole> roles = new()
         {
@@ -156,7 +156,7 @@ public class AppDbSeed
         }
         builder.Entity<Usuario>().HasData(usuarios);
         #endregion
-    
+
         #region Populate UserRole - Usuário com Perfil
         List<IdentityUserRole<string>> userRoles = new()
         {
@@ -173,7 +173,7 @@ public class AppDbSeed
         };
         builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
         #endregion
-        
+
         #region Popular Banners
         List<AmbienteImagem> ambienteImagens = new()
         {
@@ -190,6 +190,76 @@ public class AppDbSeed
                 CaminhoImagem = "/img/NossoAmbiente/amb2.png",
                 Ativo = true,
                 Ordem = 2
+            },
+            new AmbienteImagem
+            {
+                Id = 3,
+                CaminhoImagem = "/img/NossoAmbiente/amb3.png",
+                Ativo = true,
+                Ordem = 3
+            },
+            new AmbienteImagem
+            {
+                Id = 4,
+                CaminhoImagem = "/img/NossoAmbiente/amb4.png",
+                Ativo = true,
+                Ordem = 4
+            },
+            new AmbienteImagem
+            {
+                Id = 5,
+                CaminhoImagem = "/img/NossoAmbiente/amb5.png",
+                Ativo = true,
+                Ordem = 5
+            },
+            new AmbienteImagem
+            {
+                Id = 6,
+                CaminhoImagem = "/img/NossoAmbiente/amb6.png",
+                Ativo = true,
+                Ordem = 6
+            },
+            new AmbienteImagem
+            {
+                Id = 7,
+                CaminhoImagem = "/img/NossoAmbiente/amb7.png",
+                Ativo = true,
+                Ordem = 7
+            },
+            new AmbienteImagem
+            {
+                Id = 8,
+                CaminhoImagem = "/img/NossoAmbiente/amb8.png",
+                Ativo = true,
+                Ordem = 8
+            },
+            new AmbienteImagem
+            {
+                Id = 9,
+                CaminhoImagem = "/img/NossoAmbiente/amb9.png",
+                Ativo = true,
+                Ordem = 9
+            },
+            new AmbienteImagem
+            {
+                Id = 10,
+                CaminhoImagem = "/img/NossoAmbiente/amb10.png",
+                Ativo = true,
+                Ordem = 10
+            },
+            new AmbienteImagem
+            {
+                Id = 11,
+                CaminhoImagem = "/img/NossoAmbiente/amb11.png",
+                Ativo = true,
+                Ordem = 11
+            },
+            new AmbienteImagem
+            {
+                Id = 12,
+                CaminhoImagem = "/img/NossoAmbiente/amb12.png",
+                Ativo = true,
+                Ordem = 12
             },
         };
 
