84db9c8c1d9ecdab49a418390b5bb9f45d82f820
bgsana <bgs.ana08@gmail.com>
2026-05-28 22:29:26 -0300

    Personalizado a parte administrativa da página nosso ambiente

--- Vereda-Cafeteria/Controllers/NossoAmbienteController.cs
+++ Vereda-Cafeteria/Controllers/NossoAmbienteController.cs
@@ -27,16 +27,13 @@ public class NossoAmbienteController : Controller
     public async Task<IActionResult> Details(int? id)
     {
         if (id == null)
-        {
             return NotFound();
-        }
 
         var ambienteImagem = await _context.AmbienteImagens
             .FirstOrDefaultAsync(m => m.Id == id);
+
         if (ambienteImagem == null)
-        {
             return NotFound();
-        }
 
         return View(ambienteImagem);
     }
@@ -48,86 +45,120 @@ public class NossoAmbienteController : Controller
     }
 
     // POST: NossoAmbiente/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,CaminhoImagem,Ordem,Ativo")] AmbienteImagem ambienteImagem)
+    public async Task<IActionResult> Create(AmbienteImagem model, IFormFile imagemArquivo)
     {
+        if (imagemArquivo != null && imagemArquivo.Length > 0)
+        {
+            var nomeArquivo = Path.GetFileName(imagemArquivo.FileName);
+            var pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", "NossoAmbiente");
+
+            if (!Directory.Exists(pasta))
+                Directory.CreateDirectory(pasta);
+
+            var caminhoCompleto = Path.Combine(pasta, nomeArquivo);
+
+            using (var stream = new FileStream(caminhoCompleto, FileMode.Create))
+                await imagemArquivo.CopyToAsync(stream);
+
+            model.CaminhoImagem = "/img/NossoAmbiente/" + nomeArquivo;
+        }
+
+        // Remove CaminhoImagem do ModelState para não bloquear quando vem do upload
+        ModelState.Remove("CaminhoImagem");
+
         if (ModelState.IsValid)
         {
-            _context.Add(ambienteImagem);
+            _context.Add(model);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        return View(ambienteImagem);
+
+        return View(model);
     }
 
     // GET: NossoAmbiente/Edit/5
     public async Task<IActionResult> Edit(int? id)
     {
         if (id == null)
-        {
             return NotFound();
-        }
 
         var ambienteImagem = await _context.AmbienteImagens.FindAsync(id);
+
         if (ambienteImagem == null)
-        {
             return NotFound();
-        }
+
         return View(ambienteImagem);
     }
 
     // POST: NossoAmbiente/Edit/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("Id,CaminhoImagem,Ordem,Ativo")] AmbienteImagem ambienteImagem)
+    public async Task<IActionResult> Edit(int id, AmbienteImagem model, IFormFile imagemArquivo)
     {
-        if (id != ambienteImagem.Id)
-        {
+        if (id != model.Id)
             return NotFound();
+
+        if (imagemArquivo != null && imagemArquivo.Length > 0)
+        {
+            var nomeArquivo = Path.GetFileName(imagemArquivo.FileName);
+            var pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", "NossoAmbiente");
+
+            if (!Directory.Exists(pasta))
+                Directory.CreateDirectory(pasta);
+
+            var caminhoCompleto = Path.Combine(pasta, nomeArquivo);
+
+            using (var stream = new FileStream(caminhoCompleto, FileMode.Create))
+                await imagemArquivo.CopyToAsync(stream);
+
+            model.CaminhoImagem = "/img/NossoAmbiente/" + nomeArquivo;
         }
+        else
+        {
+            // Mantém o caminho já salvo no banco se nenhuma imagem nova for enviada
+            var existente = await _context.AmbienteImagens.AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existente != null)
+                model.CaminhoImagem = existente.CaminhoImagem;
+        }
+
+        ModelState.Remove("CaminhoImagem");
 
         if (ModelState.IsValid)
         {
             try
             {
-                _context.Update(ambienteImagem);
+                _context.Update(model);
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!AmbienteImagemExists(ambienteImagem.Id))
-                {
+                if (!AmbienteImagemExists(model.Id))
                     return NotFound();
-                }
                 else
-                {
                     throw;
-                }
             }
+
             return RedirectToAction(nameof(Index));
         }
-        return View(ambienteImagem);
+
+        return View(model);
     }
 
     // GET: NossoAmbiente/Delete/5
     public async Task<IActionResult> Delete(int? id)
     {
         if (id == null)
-        {
             return NotFound();
-        }
 
         var ambienteImagem = await _context.AmbienteImagens
             .FirstOrDefaultAsync(m => m.Id == id);
+
         if (ambienteImagem == null)
-        {
             return NotFound();
-        }
 
         return View(ambienteImagem);
     }
@@ -138,10 +169,9 @@ public class NossoAmbienteController : Controller
     public async Task<IActionResult> DeleteConfirmed(int id)
     {
         var ambienteImagem = await _context.AmbienteImagens.FindAsync(id);
+
         if (ambienteImagem != null)
-        {
             _context.AmbienteImagens.Remove(ambienteImagem);
-        }
 
         await _context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
